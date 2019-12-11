f42f9eca02bf318222f0eae51e231ca578e2f811
phvinayak017 <vinayak.hulabutti@gmail.com>
2019-12-11 18:46:10 +0530

    first

---
+++ graphql/src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Portfolio = ()=> {
+    return (
+        <div>
+            <h2>
+                Portfolio work
+            </h2>
+            <button>
+                Portfolio
+            </button>
+        </div>
+    )
+}
