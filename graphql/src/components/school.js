8d16a0ba2b47454a9d880562ae42dd9aafe4b63a
phvinayak017 <vinayak.hulabutti@gmail.com>
2019-12-11 20:03:43 +0530

    first

---
+++ graphql/src/components/school.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const School = ()=> {
+    return (
+        <div>
+            <h2>
+                School work
+            </h2>
+            <button>
+                School
+            </button>
+        </div>
+    )
+}
