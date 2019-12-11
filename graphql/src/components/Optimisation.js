a5f4cd254b13352d5bfa85f2bfdf3ed268d85385
phvinayak017 <vinayak.hulabutti@gmail.com>
2019-12-11 20:11:29 +0530

    first

---
+++ graphql/src/components/Optimisation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Optimisation = ()=> {
+    return (
+        <div>
+            <h2>
+            Optimisation work
+            </h2>
+            <button>
+            Optimisation
+            </button>
+        </div>
+    )
+}
